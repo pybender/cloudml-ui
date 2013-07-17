d96ce89f85179396641f899a685c0683f58f19b2
Nikolay Melnik <nmelnik@odesk.com>
2013-07-17 13:12:33 +0300

    Adding manage py command for check Tests and Models

--- manage.py
+++ manage.py
@@ -1,6 +1,10 @@
 import os
 
-from flask.ext.script import Manager, Command, Shell
+from flask.ext.script import Manager, Command, Shell, Option
+
+import logging
+from bson.objectid import ObjectId
+from mongokit.mongo_exceptions import AutoReferenceError
 
 from api import app
 
@@ -85,6 +89,96 @@ class Test(Command):
         nose.run(argv=['', '--exclude-dir=core'])
 
 
+class RemObsoluteMongoKeys(Command):
+    """
+    Removes obsolete (hung ups) Tests, Examples, Logs, etc. from
+    mongodb.
+    """
+    TYPE_CHOICES = ('models', 'tests')
+
+    option_list = (
+        Option('--obj', '-o', dest='obj_type', default='all'),
+    )
+
+    def run(self, obj_type):
+        def execute(obj_type):
+            mthd = getattr(self, "_check_%s" % obj_type)
+            mthd()
+
+        if obj_type != 'all':
+            execute(obj_type)
+        else:
+            for obj_type in self.TYPE_CHOICES:
+                execute(obj_type)
+
+    def _check_models(self):
+        logging.info("================ Analyse Models ================")
+        for i, model in enumerate(app.db.Model.find()):
+            logging.info('%d - Model: %s', i, model.name)
+
+            def _check_handler(handler_type='test'):
+                hander = model.get('%s_import_handler' % handler_type)
+                if not hander:
+                    logging.warning('%s Handler not set', handler_type)
+
+            _check_handler('test')
+            _check_handler('train')
+
+            train_handler_dict = model.get('train_importhandler')
+            if train_handler_dict:
+                logging.warning('Train import handler dict is here!')
+
+            importhandler_dict = model.get('importhandler')
+            if importhandler_dict:
+                logging.warning('Test import handler dict is here!')
+
+    def _check_tests(self):
+        logging.info("================ Analyse Tests ================")
+        fields = ('name', '_id', 'model_name',
+                  'model_id', 'created_on')
+        for i, test in enumerate(app.db.Test.find({}, fields)):
+            model_name = test.get('model_name', None)
+            model_id = test.get('model_id', None)
+            logging.info("%d - Test: %s. Created: %s", i, test._id, test.created_on)
+            logging.info('Looking for model by id')
+            if model_id:
+                model = app.db.Model.find_one({'_id': ObjectId(model_id)})
+                if not model:
+                    logging.error('Model not found by id %s' % model_id)
+                else:
+                    logging.info('Found: %s', model.name)
+            else:
+                logging.warning('Model id not set.')
+
+            logging.info('Looking for model by name')
+            if model_name:
+                model = app.db.Model.find_one({'name': model_name})
+                if not model:
+                    logging.error('Model not found by name %s' % model_name)
+                else:
+                    logging.info('Found: %s', model.name)
+            else:
+                logging.warning('Model name not set.')
+
+            test_obj = None
+            try:
+                test_obj = app.db.Test.find_one({'_id': test._id})
+                if test_obj.status == app.db.Test.STATUS_ERROR:
+                    logging.warning('Test executed with error %s', test_obj.error)
+                if test_obj.status == app.db.Test.STATUS_QUEUED:
+                    logging.warning('Test is queued. Please check whether it isn"t hung ups.')
+            except AutoReferenceError, exc:
+                logging.error('Problem with db refs: %s', exc)
+
+            if test_obj:
+                ds = test_obj.get('dataset', None)
+                if ds:
+                    logging.info('Dataset found %s', ds.name)
+                else:
+                    logging.warning('DataSet not set')
+
+            # Analyse Logs
+
 manager = Manager(app)
 manager.add_command("celeryd", Celeryd())
 manager.add_command("celeryw", Celeryw())
@@ -92,6 +186,7 @@ manager.add_command("flower", Flower())
 manager.add_command('test', Test())
 manager.add_command('migrate', Migrate())
 manager.add_command('run', Run())
+manager.add_command('fix_mongo', RemObsoluteMongoKeys())
 manager.add_command("shell", Shell(make_context=_make_context))
 
 if __name__ == "__main__":
