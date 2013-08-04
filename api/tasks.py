a2811c4bfbe1e2b37bc500dd3a083fdc518af658
Nikolay Melnik <nmelnik@odesk.com>
2013-08-04 16:43:51 +0300

    Update prod config

--- api/tasks.py
+++ api/tasks.py
@@ -441,8 +441,9 @@ def run_test(dataset_id, test_id):
 
 
         def _chunks(sequences, n):
-            for i in xrange(0, len(sequences[0]), int(len(sequences[0])/n)):
-                yield [s[i:i+n] for s in sequences]
+            count = int(len(sequences[0])/n)
+            for i in xrange(0, len(sequences[0]), count):
+                yield [s[i:i+count] for s in sequences]
 
         examples = [
              range(len(raw_data)),  # indexes
