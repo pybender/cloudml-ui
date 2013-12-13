d2cfa70f1e048010206234e7cb41a6ec331c55dc
Nikolay Melnik <nmelnik@odesk.com>
2013-12-13 07:42:36 +0200

    Changed test db name

--- api/test_config.py
+++ api/test_config.py
@@ -1,6 +1,6 @@
 DATABASE_NAME = 'cloudml-test-db'
 DATA_FOLDER = 'test_data'
 
-DB_NAME = 'db'
+DB_NAME = 'test-db'
 DB_FORMAT_URI = 'postgresql://postgres:postgres@localhost:5434/%s'
 SQLALCHEMY_DATABASE_URI = DB_FORMAT_URI % DB_NAME
