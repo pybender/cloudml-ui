9f33be20e25d4f3a318076ad3602689eca82e753
Mikhail Medvedev <moorcock84@gmail.com>
2014-03-31 18:43:40 +0400

    Xml handler tests

--- api/import_handlers/tests/xml_import_handlers.py
+++ api/import_handlers/tests/xml_import_handlers.py
@@ -2,7 +2,9 @@ import uuid
 import json
 
 from api.base.test_utils import BaseDbTestCase, TestChecksMixin, HTTP_HEADERS
-from ..views import XmlImportHandlerResource, XmlEntityResource
+from ..views import XmlImportHandlerResource, XmlEntityResource,\
+    XmlDataSourceResource, XmlInputParameterResource, XmlFieldResource,\
+    XmlQueryResource, XmlScriptResource
 from ..models import XmlImportHandler, XmlDataSource,\
     XmlScript, XmlField, XmlEntity, XmlInputParameter, XmlQuery
 
@@ -43,6 +45,12 @@ class XmlImportHandlerTests(BaseDbTestCase, TestChecksMixin):
         self.assertTrue('<field jsonpath="$.op_country" name="employer.country" type="string"/>'
                         in obj['xml'])
 
+        resp = self.check_details(show='name,entities', obj=self.obj)
+        obj = resp[self.RESOURCE.OBJECT_NAME]
+        self.assertTrue('fields' in obj['entity'])
+        self.assertEqual(2, len(obj['entity']['fields']))
+        self.assertEqual(3, len(obj['entity']['entities']))
+
     def test_edit_name(self):
         data = {"name": "new name"}
         resp, obj = self.check_edit(data, id=self.obj.id)
@@ -68,16 +76,8 @@ class XmlImportHandlerTests(BaseDbTestCase, TestChecksMixin):
             self.assertEqual(model.query.count(), count_all - count)
 
 
-class XmlEntityTests(BaseDbTestCase, TestChecksMixin):
-    """
-    Tests of the XML Entities API.
-    """
-    BASE_URL = '/cloudml/xml_import_handlers/'
-    RESOURCE = XmlEntityResource
-    Model = XmlEntity
-
-    def setUp(self):
-        super(XmlEntityTests, self).setUp()
+class IHLoadMixin(object):
+    def load_import_handler(self):
         name = str(uuid.uuid1())
         with open('conf/extract.xml', 'r') as fp:
             resp_data = self.client.post(
@@ -88,10 +88,23 @@ class XmlEntityTests(BaseDbTestCase, TestChecksMixin):
                 },
                 headers=HTTP_HEADERS)
             resp_data = json.loads(resp_data.data)
-        handler_id = resp_data[XmlImportHandlerResource.OBJECT_NAME]['id']
+        return resp_data[XmlImportHandlerResource.OBJECT_NAME]['id']
+
+
+class XmlEntityTests(BaseDbTestCase, TestChecksMixin, IHLoadMixin):
+    """
+    Tests of the XML Entities API.
+    """
+    BASE_URL = ''
+    RESOURCE = XmlEntityResource
+    Model = XmlEntity
+
+    def setUp(self):
+        super(XmlEntityTests, self).setUp()
+        handler_id = self.load_import_handler()
         self.BASE_URL = '/cloudml/xml_import_handlers/{0!s}/entities/'.format(
             handler_id)
-        self.obj = self.Model.query.filter_by(import_handler_id=handler_id,
+        self.obj = XmlEntity.query.filter_by(import_handler_id=handler_id,
                                               name='application').one()
 
     def test_delete(self):
@@ -116,3 +129,134 @@ class XmlEntityTests(BaseDbTestCase, TestChecksMixin):
         self.assertEqual(XmlField.query.count(), field_count - 11)
         self.assertEqual(XmlEntity.query.count(), entity_count - 3)
         self.assertEqual(XmlQuery.query.count(), query_count - 1)
+
+    def test_list(self):
+        self.check_list(show='name')
+
+    def test_details(self):
+        resp = self.check_details(show='name', obj=self.obj)
+        obj = resp[self.RESOURCE.OBJECT_NAME]
+
+
+class XmlDataSourceTests(BaseDbTestCase, TestChecksMixin, IHLoadMixin):
+    """
+    Tests of the XML Datasources API.
+    """
+    BASE_URL = ''
+    RESOURCE = XmlDataSourceResource
+    Model = XmlDataSource
+
+    def setUp(self):
+        super(XmlDataSourceTests, self).setUp()
+        handler_id = self.load_import_handler()
+        self.BASE_URL = '/cloudml/xml_import_handlers/{0!s}/datasources/'.format(
+            handler_id)
+        self.obj = XmlDataSource.query.filter_by(import_handler_id=handler_id,
+                                                 name='odw').one()
+
+    def test_list(self):
+        self.check_list(show='name')
+
+    def test_details(self):
+        resp = self.check_details(show='name', obj=self.obj)
+        obj = resp[self.RESOURCE.OBJECT_NAME]
+
+
+class XmlInputParameterTests(BaseDbTestCase, TestChecksMixin, IHLoadMixin):
+    """
+    Tests of the XML InputParameter API.
+    """
+    BASE_URL = ''
+    RESOURCE = XmlInputParameterResource
+    Model = XmlInputParameter
+
+    def setUp(self):
+        super(XmlInputParameterTests, self).setUp()
+        handler_id = self.load_import_handler()
+        self.BASE_URL = '/cloudml/xml_import_handlers/{0!s}/input_parameters/'.format(
+            handler_id)
+        self.obj = XmlInputParameter.query.filter_by(import_handler_id=handler_id,
+                                                     name='start').one()
+
+    def test_list(self):
+        self.check_list(show='name')
+
+    def test_details(self):
+        resp = self.check_details(show='name', obj=self.obj)
+        obj = resp[self.RESOURCE.OBJECT_NAME]
+
+
+class XmlScriptTests(BaseDbTestCase, TestChecksMixin, IHLoadMixin):
+    """
+    Tests of the XML Script API.
+    """
+    BASE_URL = ''
+    RESOURCE = XmlScriptResource
+    Model = XmlScript
+
+    def setUp(self):
+        super(XmlScriptTests, self).setUp()
+        handler_id = self.load_import_handler()
+        self.BASE_URL = '/cloudml/xml_import_handlers/{0!s}/scripts/'.format(
+            handler_id)
+        self.obj = XmlScript.query.filter_by(import_handler_id=handler_id).one()
+
+    def test_list(self):
+        self.check_list(show='data')
+
+    def test_details(self):
+        resp = self.check_details(show='data', obj=self.obj)
+        obj = resp[self.RESOURCE.OBJECT_NAME]
+
+
+class XmlFieldTests(BaseDbTestCase, TestChecksMixin, IHLoadMixin):
+    """
+    Tests of the XML Field API.
+    """
+    BASE_URL = ''
+    RESOURCE = XmlFieldResource
+    Model = XmlField
+
+    def setUp(self):
+        super(XmlFieldTests, self).setUp()
+        handler_id = self.load_import_handler()
+        self.entity = XmlEntity.query.filter_by(import_handler_id=handler_id,
+                                                name='application').one()
+        self.BASE_URL = '/cloudml/xml_import_handlers/{0!s}/entities/{1!s}/fields/'.format(
+            handler_id, self.entity.id)
+        self.obj = XmlField.query.filter_by(entity_id=self.entity.id,
+                                            name='application_id').one()
+
+    def test_list(self):
+        self.check_list(show='name', query_params={
+            'entity_id': self.entity.id
+        })
+
+    def test_details(self):
+        resp = self.check_details(show='name', obj=self.obj)
+        obj = resp[self.RESOURCE.OBJECT_NAME]
+
+
+class XmlQueryTests(BaseDbTestCase, TestChecksMixin, IHLoadMixin):
+    """
+    Tests of the XML Query API.
+    """
+    BASE_URL = ''
+    RESOURCE = XmlQueryResource
+    Model = XmlQuery
+
+    def setUp(self):
+        super(XmlQueryTests, self).setUp()
+        handler_id = self.load_import_handler()
+        self.entity = XmlEntity.query.filter_by(import_handler_id=handler_id,
+                                                name='application').one()
+        self.BASE_URL = '/cloudml/xml_import_handlers/{0!s}/entities/{1!s}/queries/'.format(
+            handler_id, self.entity.id)
+        self.obj = self.entity.query_obj
+
+    def test_list(self):
+        self.check_list(show='text')
+
+    def test_details(self):
+        resp = self.check_details(show='text', obj=self.obj)
+        obj = resp[self.RESOURCE.OBJECT_NAME]
