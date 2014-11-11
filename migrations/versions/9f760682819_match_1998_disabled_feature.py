42b1414a1978677b07a6b6c56262b05f687171c5
Nikolay Melnik <nmelnik@odesk.com>
2014-11-11 16:01:02 +0200

    Fixed migration

--- migrations/versions/9f760682819_match_1998_disabled_feature.py
+++ migrations/versions/9f760682819_match_1998_disabled_feature.py
@@ -8,7 +8,7 @@ Create Date: 2014-11-09 11:53:02.442243
 
 # revision identifiers, used by Alembic.
 revision = '9f760682819'
-down_revision = 'aeb5a592587'
+down_revision = '5adcc1a31357'
 
 from alembic import op
 import sqlalchemy as sa
