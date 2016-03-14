e136b931ee640327bdf5d8ab5d8da59e20a7fc7c
Nikolay Melnik <nmelnik@odesk.com>
2016-03-14 07:46:01 +0200

    MATCH-3495 ServerModelVerification resource tests

--- api/servers/fixtures.py
+++ api/servers/fixtures.py
@@ -1,4 +1,6 @@
 from fixture import DataSet
+from api.ml_models.fixtures import ModelData
+from api.model_tests.fixtures import TestResultData
 
 
 class ServerData(DataSet):
@@ -13,3 +15,13 @@ class ServerData(DataSet):
         ip = '127.0.0.2'
         folder = 'odesk-match-cloudml/local'
         is_default = True
+
+
+class ServerModelVerificationData(DataSet):
+    class model_verification_01:
+        status = 'New'
+        model = ModelData.model_01
+        test_result = TestResultData.test_01
+        server = ServerData.server_01
+        params_map = {"application": "app"}
+        error = ""
