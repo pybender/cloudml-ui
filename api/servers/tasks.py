1f023a476e79cf743db4a4c81bf76a74f6b126cb
kolia1985 <kolia1985@gmail.com>
2016-08-01 11:12:07 +0300

    Changed upload models to server

--- api/servers/tasks.py
+++ api/servers/tasks.py
@@ -62,11 +62,12 @@ def upload_model_to_server(server_id, model_id, user_id):
         'uploaded_on': str(datetime.now())
     }
 
-    trainer_data = model.trainer
-    from cloudml.trainer.store import load_trainer
+    #trainer = model.get_trainer()
+    #from cloudml.trainer.store import load_trainer
     trainer = load_trainer(trainer_data)
     trainer.visualization = None
     trainer_data = store_trainer(trainer)
+    #trainer_data = model.trainer
     s3.save_key_string(path, trainer_data, meta)
     s3.close()
     model.locked = True
