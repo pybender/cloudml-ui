7ccf85d964d9b3232d49e66c9ab6045dc70c4075
pnvasko <pnvasko@gmail.com>
2016-10-29 07:16:06 +0700

    Add scenarios task loger

--- api/schedule/tasks.py
+++ api/schedule/tasks.py
@@ -1,31 +1,64 @@
+import time
+import functools
+from celery import current_task
+from celery.utils.log import get_logger, get_task_logger
+
 from api import celery, app
 from api.base.tasks import SqlAlchemyTask
-from celery.utils.log import get_logger, get_task_logger
 from api.schedule.models import PeriodicTaskScenarios
-import time
+
 logger = get_logger(__name__)
 
+class UnwrapArgs(object):
+    def __init__(self, contents):
+        self.contents = contents
+
+    def __call__(self):
+        return self.contents
+
+def wrapchain(f):
+    @functools.wraps(f)
+    def _wrapper(*args, **kwargs):
+        if type(args[0]) == UnwrapArgs:
+            inargs = list(args[0]())
+            lastargs = args[1:]
+            args = []
+            for a in inargs:
+                if isinstance(a, dict):
+                    kwargs.update(a)
+                elif isinstance(a, (list, tuple)):
+                    args.extend(a)
+                else:
+                    args.append(a)
+            args = list(args) + list(lastargs)
+        result = f(*args, **kwargs)
+        if type(result) == tuple and current_task.request.callbacks:
+            return UnwrapArgs(result)
+        else:
+            return result
+    return _wrapper
+
+@celery.task()
+@wrapchain
+def scenarios_task_loger(*args, **kwargs):
+    scenarios_task_loger_arg = {}
+    if 'scenarios_task_loger_arg' in kwargs:
+        scenarios_task_loger_arg = kwargs.get('scenarios_task_loger_arg', {})
+        del kwargs['scenarios_task_loger_arg']
+    scenarios = scenarios_task_loger_arg.get('scenarios', 'Scenario.')
+    chainname = scenarios_task_loger_arg.get('chainname', 'Chainname.')
+    name = scenarios_task_loger_arg.get('name', 'funct')
+    text = scenarios_task_loger_arg.get('text', '...')
+    logger.error("Scenarios: {0} Chain: {1} Task in chain: {2} Info: {3}".format(scenarios, chainname, name, text))
+    return UnwrapArgs((args, kwargs))
+
 @celery.task(base=SqlAlchemyTask)
-def test_task_01(*args, **kwargs):
+@wrapchain
+def test_task(*args, **kwargs):
     name = kwargs.get('name', 'None')
     time.sleep(15)
     print("single:", args, kwargs)
     return name
-
-@celery.task(base=SqlAlchemyTask)
-@app.regscheduletask()
-def test_schedule_task():
-    tasklog = get_task_logger(__name__)
-    tasklog.debug('Start...')
-    for i in range(0, 1000000):
-        k = i * i
-    logger.debug('Test %s completed.' % 'test_schedule_task')
-    return 'test_schedule_task completed'
-
-@celery.task(base=SqlAlchemyTask)
-def scenarios_task_loger(*args, **kwargs):
-
-    return args, kwargs
 
 @celery.task(base=SqlAlchemyTask)
 @app.regscheduletask()
@@ -42,11 +75,9 @@ def scenarios_schedule_task(*args, **kwargs):
         if task.task_parser():
             logger.info('Start task %s .' % task.name)
             result = task.sctasks.delay()
-            # result = result.get()
-            # while not result.ready(): print ("wait result") time.sleep(2)
         logger.info('Task end.')
-        return 'Completed'
+        return 'Completed.'
     except Exception as e:
         logger.error('Error error processing scenarios task (%s).' % e)
-        return 'Error.ProcessingTask'
+        return 'Error.'
 
